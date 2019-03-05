9987824dc22761830fc266757e3deebf4de04a51
henrique <henriquelucas09@hotmail.com>
2019-03-05 13:44:10 -0300

    Diminuição da complexidades das redes (diminuição da quantidade de neurônios  de 67 prar 50)

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Neuronio():
         if(rows==[]):
             conn.execute("insert into PESOS(GE,TI,RE,BI,P1,P2,P3,P4) values('" + str(ge) + "','" + str(tipo) + "','" + str(rede) + "'," + str(uniform(1, 0)) +","+ str(uniform(1, 0)) +","+ str(uniform(1, 0))+","+ str(uniform(1, 0))+","+ str(uniform(1, 0))+")")
             conn.commit()
-            # if(tipo>1 or ide==(((r-1)*67)+1)):
+            # if(tipo>1 or ide==(((r-1)*50)+1)):
             #     if(tipo==2):
             #         conn.execute("update PESOS set P3="+str(uniform(1,0))+" where ID="+str(self.ide))
             #         conn.commit()
@@ -38,6 +38,7 @@ class Neuronio():
         c=0
         cursor = conn.execute("select BI from PESOS where ID=" + str(self.ide))
         rows = cursor.fetchall()
+        print("ID: "+str(self.ide))
         res=-0.5*rows[0][0]
         print("Bias:"+str(res))
         for i in args:
@@ -57,62 +58,62 @@ class Neuronio():
             return (0,0)
 
 def entrada(t,r,v1,v2):
-    a1=Neuronio(t,r,(((r-1)*67)+1))
-    a2=Neuronio(t,r,(((r-1)*67)+2))
+    a1=Neuronio(t,r,(((r-1)*50)+1))
+    a2=Neuronio(t,r,(((r-1)*50)+2))
     r1=a1.processamento(v1,v2)[1]
     r2=a2.processamento(v1,v2)[1]
-    a3=Neuronio(t,r,(((r-1)*67)+3))
-    a4=Neuronio(t,r,(((r-1)*67)+4))
+    a3=Neuronio(t,r,(((r-1)*50)+3))
+    a4=Neuronio(t,r,(((r-1)*50)+4))
     r3=a3.processamento(r1,r2)[1]
     r4=a4.processamento(r1,r2)[1]
-    a5=Neuronio(t,r,(((r-1)*67)+5))
+    a5=Neuronio(t,r,(((r-1)*50)+5))
     r=a5.processamento(r3,r4)[1]
     #print(r)
     return r
 
 def frente(t,r,v1,v2):
-    a1 = Neuronio(t, r,(((r-1)*67)+6))
-    a2 = Neuronio(t, r,(((r-1)*67)+7))
+    a1 = Neuronio(t, r,(((r-1)*50)+6))
+    a2 = Neuronio(t, r,(((r-1)*50)+7))
     r1 = a1.processamento(v1, v2)[1]
     r2 = a2.processamento(v1, v2)[1]
-    a3 = Neuronio(t, r,(((r-1)*67)+8))
-    a4 = Neuronio(t, r,(((r-1)*67)+9))
+    a3 = Neuronio(t, r,(((r-1)*50)+8))
+    a4 = Neuronio(t, r,(((r-1)*50)+9))
     r3 = a3.processamento(r1, r2)[1]
     r4 = a4.processamento(r1, r2)[1]
-    a5 = Neuronio(t, r,(((r-1)*67)+10))
+    a5 = Neuronio(t, r,(((r-1)*50)+10))
     r = a5.processamento(r3, r4)[1]
     #print(r)
     return r
 
 def resolucaoAt(t,re,*l):
-    a1 = Neuronio(t+1, re,(((re-1)*67)+54))
-    a2 = Neuronio(t+1, re,(((re-1)*67)+55))
-    a3 = Neuronio(t+1, re,(((re-1)*67)+56))
-    a4 = Neuronio(t+1, re,(((re-1)*67)+57))
+    # a1 = Neuronio(t+1, re,(((re-1)*50)+54))#retirar
+    # a2 = Neuronio(t+1, re,(((re-1)*50)+55))#retirar
+    # a3 = Neuronio(t+1, re,(((re-1)*50)+56))#retirar
+    # a4 = Neuronio(t+1, re,(((re-1)*50)+57))#retirar
+    # r1 = a1.processamento(l[0], l[1], l[2],l[3])[1]#retirar
+    # r2 = a2.processamento(l[0], l[1], l[2],l[3])[1]#retirar
+    # r3 = a3.processamento(l[0], l[1], l[2],l[3])[1]#retirar
+    # r4 = a4.processamento(l[0], l[1], l[2],l[3])[1]#retirar
+    a1 = Neuronio(t+1, re,(((re-1)*50)+41))
+    a2 = Neuronio(t+1, re,(((re-1)*50)+42))
+    a3 = Neuronio(t+1, re,(((re-1)*50)+43))
+    a4 = Neuronio(t+1, re,(((re-1)*50)+44))
     r1 = a1.processamento(l[0], l[1], l[2],l[3])[1]
     r2 = a2.processamento(l[0], l[1], l[2],l[3])[1]
     r3 = a3.processamento(l[0], l[1], l[2],l[3])[1]
     r4 = a4.processamento(l[0], l[1], l[2],l[3])[1]
-    a5 = Neuronio(t+1, re,(((re-1)*67)+58))
-    a6 = Neuronio(t+1, re,(((re-1)*67)+59))
-    a7 = Neuronio(t+1, re,(((re-1)*67)+60))
-    a8 = Neuronio(t+1, re,(((re-1)*67)+61))
+    a5 = Neuronio(t+1, re,(((re-1)*50)+45))
+    a6 = Neuronio(t+1, re,(((re-1)*50)+46))
+    a7 = Neuronio(t+1, re,(((re-1)*50)+47))
     r5 = a5.processamento(r1, r2, r3, r4)[1]
     r6 = a6.processamento(r1, r2, r3, r4)[1]
     r7 = a7.processamento(r1, r2, r3, r4)[1]
-    r8 = a8.processamento(r1, r2, r3, r4)[1]
-    a9 = Neuronio(t+1, re,(((re-1)*67)+62))
-    a10 = Neuronio(t+1, re,(((re-1)*67)+63))
-    a11 = Neuronio(t+1, re,(((re-1)*67)+64))
-    r9 = a9.processamento(r5, r6, r7, r8)[1]
-    r10 = a10.processamento(r5, r6, r7, r8)[1]
-    r11 = a11.processamento(r5, r6, r7, r8)[1]
-    a12 = Neuronio(t, re,(((re-1)*67)+65))
-    a13 = Neuronio(t, re,(((re-1)*67)+66))
-    r12 = a12.processamento(r9, r10, r11)[1]
-    r13 = a13.processamento(r9, r10, r11)[1]
-    a14 = Neuronio(t-1, re,(((re-1)*67)+67))
-    r = a14.processamento(r12, r13)[1]
+    a8 = Neuronio(t, re,(((re-1)*50)+48))
+    a9 = Neuronio(t, re,(((re-1)*50)+49))
+    r8 = a8.processamento(r5, r6, r7)[1]
+    r9 = a9.processamento(r5, r6, r7)[1]
+    a10 = Neuronio(t-1, re,(((re-1)*50)+50))
+    r = a10.processamento(r8, r9)[1]
     return r
 
 
@@ -120,55 +121,55 @@ def atras(t,re,*l):
     lisRes=[]
     c=0
     while(c<9):
-        a1 = Neuronio(t,re,(((re-1)*67)+11+(10*c/3)))
-        a2 = Neuronio(t,re,(((re-1)*67)+12+(10*c/3)))
-        a3 = Neuronio(t,re,(((re-1)*67)+13+(10*c/3)))
+        # a1 = Neuronio(t,re,(((re-1)*50)+11+(10*c/3)))#retirar
+        # a2 = Neuronio(t,re,(((re-1)*50)+12+(10*c/3)))#retirar
+        # a3 = Neuronio(t,re,(((re-1)*50)+13+(10*c/3)))#retirar
+        # r1 = a1.processamento(l[c],l[c+1],l[c+2])[1]#retirar
+        # r2 = a2.processamento(l[c],l[c+1],l[c+2])[1]#retirar
+        # r3 = a3.processamento(l[c],l[c+1],l[c+2])[1]#retirar
+        a1 = Neuronio(t,re,(((re-1)*50)+11+(7*c/3)))
+        a2 = Neuronio(t,re,(((re-1)*50)+12+(7*c/3)))
+        a3 = Neuronio(t,re,(((re-1)*50)+13+(7*c/3)))
         r1 = a1.processamento(l[c],l[c+1],l[c+2])[1]
         r2 = a2.processamento(l[c],l[c+1],l[c+2])[1]
         r3 = a3.processamento(l[c],l[c+1],l[c+2])[1]
-        a4 = Neuronio(t,re,(((re-1)*67)+14+(10*c/3)))
-        a5 = Neuronio(t,re,(((re-1)*67)+15+(10*c/3)))
-        a6 = Neuronio(t,re,(((re-1)*67)+16+(10*c/3)))
+        a4 = Neuronio(t,re,(((re-1)*50)+14+(7*c/3)))
+        a5 = Neuronio(t,re,(((re-1)*50)+15+(7*c/3)))
+        a6 = Neuronio(t,re,(((re-1)*50)+16+(7*c/3)))
         r4 = a4.processamento(r1, r2, r3)[1]
         r5 = a5.processamento(r1, r2, r3)[1]
         r6 = a6.processamento(r1, r2, r3)[1]
-        a7 = Neuronio(t,re,(((re-1)*67)+17+(10*c/3)))
-        a8 = Neuronio(t,re,(((re-1)*67)+18+(10*c/3)))
-        a9 = Neuronio(t,re,(((re-1)*67)+19+(10*c/3)))
-        r7 = a7.processamento(r4, r5, r6)[1]
-        r8 = a8.processamento(r4, r5, r6)[1]
-        r9 = a9.processamento(r4, r5, r6)[1]
-        a10 = Neuronio(t,re,(((re-1)*67)+20+(10*c/3)))
-        r = a10.processamento(r7, r8, r9)[1]
+        a7 = Neuronio(t,re,(((re-1)*50)+17+(7*c/3)))
+        r = a7.processamento(r4, r5, r6)[1]
         lisRes+=[r]
         #print(r)
         c+=3
-    a1 = Neuronio(t, re,(((re-1)*67)+41))
-    a2 = Neuronio(t, re,(((re-1)*67)+42))
-    a3 = Neuronio(t, re,(((re-1)*67)+43))
-    a4 = Neuronio(t, re,(((re-1)*67)+44))
+    # a1 = Neuronio(t, re,(((re-1)*50)+41))#retirar
+    # a2 = Neuronio(t, re,(((re-1)*50)+42))#retirar
+    # a3 = Neuronio(t, re,(((re-1)*50)+43))#retirar
+    # a4 = Neuronio(t, re,(((re-1)*50)+44))#retirar
+    # r1 = a1.processamento(l[9], l[10], l[11])[1]#retirar
+    # r2 = a2.processamento(l[9], l[10], l[11])[1]#retirar
+    # r3 = a3.processamento(l[9], l[10], l[11])[1]#retirar
+    # r4 = a4.processamento(l[9], l[10], l[11])[1]#retirar
+    a1 = Neuronio(t+1, re,(((re-1)*50)+32))
+    a2 = Neuronio(t+1, re,(((re-1)*50)+33))
+    a3 = Neuronio(t+1, re,(((re-1)*50)+34))
+    a4 = Neuronio(t+1, re,(((re-1)*50)+35))
     r1 = a1.processamento(l[9], l[10], l[11])[1]
     r2 = a2.processamento(l[9], l[10], l[11])[1]
     r3 = a3.processamento(l[9], l[10], l[11])[1]
     r4 = a4.processamento(l[9], l[10], l[11])[1]
-    a5 = Neuronio(t+1, re,(((re-1)*67)+45))
-    a6 = Neuronio(t+1, re,(((re-1)*67)+46))
-    a7 = Neuronio(t+1, re,(((re-1)*67)+47))
-    a8 = Neuronio(t+1, re,(((re-1)*67)+48))
+    a5 = Neuronio(t+1, re,(((re-1)*50)+36))
+    a6 = Neuronio(t+1, re,(((re-1)*50)+37))
+    a7 = Neuronio(t+1, re,(((re-1)*50)+38))
+    a8 = Neuronio(t+1, re,(((re-1)*50)+39))
     r5 = a5.processamento(r1, r2, r3,r4)[1]
     r6 = a6.processamento(r1, r2, r3,r4)[1]
     r7 = a7.processamento(r1, r2, r3,r4)[1]
     r8 = a8.processamento(r1, r2, r3,r4)[1]
-    a9 = Neuronio(t+1, re,(((re-1)*67)+49))
-    a10 = Neuronio(t+1, re,(((re-1)*67)+50))
-    a11 = Neuronio(t+1, re,(((re-1)*67)+51))
-    a12 = Neuronio(t+1, re,(((re-1)*67)+52))
-    r9 = a9.processamento(r5, r6, r7,r8)[1]
-    r10 = a10.processamento(r5, r6, r7,r8)[1]
-    r11= a11.processamento(r5, r6, r7,r8)[1]
-    r12 = a12.processamento(r5, r6, r7,r8)[1]
-    a13 = Neuronio(t+1, re,(((re-1)*67)+53))
-    r = a13.processamento(r9, r10, r11,r12)[1]
+    a13 = Neuronio(t+1, re,(((re-1)*50)+40))
+    r = a13.processamento(r5, r6, r7,r8)[1]
     lisRes+=[r]
     r=resolucaoAt(t,re,*lisRes)
     return r
@@ -211,7 +212,7 @@ def redes():
                 print("vai atras")
                 if(at<0.25):
                     print("primeira fileira")
-                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r - 1) * 67) + 1)))
+                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r - 1) * 50) + 1)))
                     rows = cursor.fetchall()
                     at *= rows[0][0]
                     if(at<1/3):
@@ -225,7 +226,7 @@ def redes():
                         dicRes[pri][1][2] = 1
                 elif(at<0.5):
                     print("segunda fileira")
-                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r - 1) * 67) + 1)))
+                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r - 1) * 50) + 1)))
                     rows = cursor.fetchall()
                     at *= rows[0][0]
                     if (at < 1 / 3):
@@ -239,7 +240,7 @@ def redes():
                         dicRes[pri][2][2] = 1
                 elif(at<0.75):
                     print("terceira fileira")
-                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r - 1) * 67) + 1)))
+                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r - 1) * 50) + 1)))
                     rows = cursor.fetchall()
                     at *= rows[0][0]
                     if (at < 1 / 3):
@@ -253,7 +254,7 @@ def redes():
                         dicRes[pri][3][2] = 1
                 else:
                     print("quarta fileira")
-                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r-1)*67)+1)))
+                    cursor = conn.execute("select P3 from PESOS where ID=" + str((((r-1)*50)+1)))
                     rows = cursor.fetchall()
                     at*=rows[0][0]
                     if (at < 0.25):
@@ -274,17 +275,17 @@ def redes():
 
 def registra(i,c,f,mp,g,des):
     if (des>0.5):# des==0.5
-        conn.execute("update PESOS set " + str(i) + "=" + str(uniform(1, 0)) + " where ID=" + str((67 * c) + f + 1))
+        conn.execute("update PESOS set " + str(i) + "=" + str(uniform(1, 0)) + " where ID=" + str((50 * c) + f + 1))
         conn.commit()#mutação
-        conn.execute("update PESOS set GE='" + str((g)) + "' where ID=" + str((67 * c) + f + 1))
+        conn.execute("update PESOS set GE='" + str((g)) + "' where ID=" + str((50 * c) + f + 1))
         conn.commit()
-        print("ISSO:" + str((67 * c) + f + 1))
+        print("ISSO:" + str((50 * c) + f + 1))
     else:
-        conn.execute("update PESOS set " + str(i) + "=" + str(mp[i][f][0]) + " where ID=" + str((67 * c) + f + 1))
+        conn.execute("update PESOS set " + str(i) + "=" + str(mp[i][f][0]) + " where ID=" + str((50 * c) + f + 1))
         conn.commit()
-        conn.execute("update PESOS set GE='" + str((g)) + "' where ID=" + str((67 * c) + f + 1))
+        conn.execute("update PESOS set GE='" + str((g)) + "' where ID=" + str((50 * c) + f + 1))
         conn.commit()
-        print("ISSO:" + str((67 * c) + f + 1))
+        print("ISSO:" + str((50 * c) + f + 1))
 
 def crossing(pai,mae,dic):
     g=int(ge)
@@ -292,25 +293,25 @@ def crossing(pai,mae,dic):
     p = {}
     m = {}
     cursor = conn.execute("select BI from PESOS")
-    p['bi'] = cursor.fetchall()[((pai * 67)):((pai * 67) + 67)]
+    p['bi'] = cursor.fetchall()[((pai * 50)):((pai * 50) + 50)]
     cursor = conn.execute("select BI from PESOS")
-    m['bi'] = cursor.fetchall()[((mae * 67)):((mae * 67) + 67)]
+    m['bi'] = cursor.fetchall()[((mae * 50)):((mae * 50) + 50)]
     cursor = conn.execute("select P1 from PESOS")
-    p['p1'] = cursor.fetchall()[((pai * 67)):((pai * 67) + 67)]
+    p['p1'] = cursor.fetchall()[((pai * 50)):((pai * 50) + 50)]
     cursor = conn.execute("select P1 from PESOS")
-    m['p1'] = cursor.fetchall()[((mae * 67)):((mae * 67) + 67)]
+    m['p1'] = cursor.fetchall()[((mae * 50)):((mae * 50) + 50)]
     cursor = conn.execute("select P2 from PESOS")
-    p['p2'] = cursor.fetchall()[((pai * 67)):((pai * 67) + 67)]
+    p['p2'] = cursor.fetchall()[((pai * 50)):((pai * 50) + 50)]
     cursor = conn.execute("select P2 from PESOS")
-    m['p2'] = cursor.fetchall()[((mae * 67)):((mae * 67) + 67)]
+    m['p2'] = cursor.fetchall()[((mae * 50)):((mae * 50) + 50)]
     cursor = conn.execute("select P3 from PESOS")
-    p['p3'] = cursor.fetchall()[((pai * 67)):((pai * 67) + 67)]
+    p['p3'] = cursor.fetchall()[((pai * 50)):((pai * 50) + 50)]
     cursor = conn.execute("select P3 from PESOS")
-    m['p3'] = cursor.fetchall()[((mae * 67)):((mae * 67) + 67)]
+    m['p3'] = cursor.fetchall()[((mae * 50)):((mae * 50) + 50)]
     cursor = conn.execute("select P4 from PESOS")
-    p['p4'] = cursor.fetchall()[((pai * 67)):((pai * 67) + 67)]
+    p['p4'] = cursor.fetchall()[((pai * 50)):((pai * 50) + 50)]
     cursor = conn.execute("select P4 from PESOS")
-    m['p4'] = cursor.fetchall()[((mae * 67)):((mae * 67) + 67)]
+    m['p4'] = cursor.fetchall()[((mae * 50)):((mae * 50) + 50)]
 
     c = 0
     a1=p['p1'][4][0] * m['p1'][4][0]
@@ -319,10 +320,10 @@ def crossing(pai,mae,dic):
         print("OLHA:"+str(c))
         f = 0#TEMOS ALGUNS PROBLEMAS COM ISSO AQUI!!!!->ele não ta aprendendo direito
 
-        while (f < 67):
+        while (f < 50):
             for i, k in m.items():
                 if ( a1> 0.5):
-                    if (int(dic[pai])==1000):#int(dic[pai])==1000
+                    if (int(dic[pai])>=2000):#int(dic[pai])==1000
                         registra(i,c,f,p,g,uniform(1,0))# randint(0, 1)
                     else:
                         if(randint(0,1)==0):
@@ -333,7 +334,7 @@ def crossing(pai,mae,dic):
 
 
                 else:
-                    if (int(dic[mae])==1000):
+                    if (int(dic[mae])>=2000):
                         registra(i,c,f,p,g,uniform(1,0))# randint(0, 1)
                     else:
                         if(randint(0,1)==0):
@@ -417,7 +418,7 @@ while(kfr<300):
     kfr+=1
 
 
-#SÃO 67 REGISTROS FEITOS NA TABELA PARA UMA REDE NEURAL COMPLETA
+#SÃO 50 REGISTROS FEITOS NA TABELA PARA UMA REDE NEURAL COMPLETA
 #print(atras(1,1, 1,1,1 ,1,1,1 ,1,1,1 ,1,1,1,1))
 
 # c=Neuronio()
